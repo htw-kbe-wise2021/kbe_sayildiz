1c50d3b16973996f5aabb651102038fffbf84d04
sayildiz <y.samet97@gmail.com>
2021-10-27 02:50:56 +0200

    create Spring Project
    Update .gitignore
    
    added out/

---
+++ Songify/src/main/java/com/sayildiz/Songify/SongifyApplication.java
@@ -0,0 +1,13 @@
+package com.sayildiz.Songify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongifyApplication.class, args);
+	}
+
+}
---
+++ Songify/src/test/java/com/sayildiz/Songify/SongifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayildiz.Songify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
